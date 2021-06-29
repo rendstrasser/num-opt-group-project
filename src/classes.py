4163ae11821534329ef5bc929e4d5400aaa70c88
Alexander <alexandertemper@protonmail.com>
2021-06-29 11:08:17 +0200

    Implement approximated gradient

--- src/classes.py
+++ src/classes.py
@@ -2,6 +2,8 @@ import numpy as np
 from typing import Callable
 from dataclasses import dataclass
 
+from numpy.lib.function_base import gradient
+
 
 @dataclass
 class MinimizationProblem:
@@ -11,28 +13,50 @@ class MinimizationProblem:
 
     Args:
         f (Callable): The function (objective) we are trying to minimize.
-        gradient_f (Callable): The gradient function of f.
-        hessian_f (Callable): The hessian function of f.
         solution (list): The solution(s) to the minimization problem.
                              Might contain multiple if there are multiple local minimizers.
         x0 (list): The starting point for the minimization procedure.
+        gradient_f (Callable): The gradient function of f. Optional.
+        hessian_f (Callable): The hessian function of f. Optional.
     """
     f: Callable[[np.ndarray], np.ndarray]
-    gradient_f: Callable[[np.ndarray], np.ndarray]
-    hessian_f: Callable[[np.ndarray], np.ndarray]
     solution: np.ndarray
     x0: np.ndarray
+    gradient_f: Callable[[np.ndarray], np.ndarray] = None
+    hessian_f: Callable[[np.ndarray], np.ndarray] = None
 
-    # TODO improve
-    def calc_gradient_at(self,
-            f: Callable[[np.ndarray], np.ndarray],
-            x: np.ndarray) -> np.ndarray:
+    # Why do we pass f, when it is saved in the state?
+    def calc_gradient_at(self, f: Callable, x: np.ndarray) -> np.ndarray:
+        """Calculate gradient at point `x`. Uses an approximation, if the gradient is not explicitly known.
 
-        if self.gradient_f is not None:
-            return self.gradient_f(x)
+        Args:
+            f (Callable): Function. 
+            x (np.ndarray): Array representing some point in the domain of the function.
 
-        # TODO
-        raise NotImplementedError()
+        Returns:
+            np.ndarray: (Approxmiated or true) gradient at point `x`.
+        """
+
+        return (self.gradient_f or self.central_difference_gradient)(x)
+
+    def central_difference_gradient(self, x: np.ndarray) -> np.ndarray:
+        """Approximate gradient as described in equation (8.7), called the 'central difference formula'.
+
+        Args:
+            x (np.ndarray): Function input.
+
+        Returns:
+            np.ndarray: Approximated gradient.
+        """
+
+        # Given the datatype of x, the below is the least number such that `1.0 + eps != 1.0`.
+        eps = np.finfo(x.dtype).eps
+
+        return np.array([
+            (self.f(x + eps*unitvector) + self.f(x - eps*unitvector))/2*self.epsilon
+            for unitvector in np.eye(N=len(x))
+        ])
+        
 
     # TODO improve (maybe)
     def calc_hessian_at(self,
